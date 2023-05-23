b5530f37e0e36e981a039842953bf28efe2473df
Asheklm6 <asheklm6@gmail.com>
2023-05-23 16:29:33 +0000

    j

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-// Write a script that reads and prints the content of a file.
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data.toString());
-  }
+const args = process.argv.slice(2);
+const fileName = args[0];
+
+fs.readFile(fileName, 'utf-8', (err, data) => {
+  if (err) console.log(err);
+  else console.log(data);
 });
